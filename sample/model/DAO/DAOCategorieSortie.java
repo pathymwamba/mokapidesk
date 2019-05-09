f706d14a304b5788922a4f9ef28d8e45dc09f2c0
pathy <pathypbmmwamba@gmail.com>
2019-05-09 14:40:53 +0200

    Crud sur DAOCategorieSortie

--- sample/model/DAO/DAOCategorieSortie.java
+++ sample/model/DAO/DAOCategorieSortie.java
@@ -2,24 +2,67 @@ package sample.model.DAO;
 
 import sample.model.CategorieSortie;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DAOCategorieSortie extends DAO<CategorieSortie> {
+
+    public DAOCategorieSortie(){
+        super();
+    }
+
     @Override
     public boolean create(CategorieSortie categorieSortie) {
-        return false;
+        try{
+            String requete = "insert into categoriesortie(nom) values('"+categorieSortie.getNom()+"')";
+            this.connexion.getStmt().executeUpdate(requete);
+            return true;
+
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean update(CategorieSortie categorieSortie) {
-        return false;
+        try{
+            String requete = "update categoriesortie set nom='"+categorieSortie.getNom()+"' WHERE idcategorie="+categorieSortie.getId();
+            this.connexion.getStmt().executeUpdate(requete);
+            return true;
+
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean delete(CategorieSortie categorieSortie) {
-        return false;
+        try{
+            String requete = "delete from categoriesortie WHERE idcategorie="+categorieSortie.getId();
+            this.connexion.getStmt().executeUpdate(requete);
+            return true;
+
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public CategorieSortie find(int id) {
+        CategorieSortie cats;
+        try{
+            String requete ="select * from categoriesortie WHERE idcategorie="+id;
+            ResultSet result = this.connexion.getStmt().executeQuery(requete);
+            if(result.first()){
+                cats = new CategorieSortie(Integer.parseInt(result.getString("idcategorie")), result.getString("nom"));
+                return cats;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
         return null;
     }
 }
