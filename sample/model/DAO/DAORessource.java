3d0c6cb790dae5427f412f6f22fe3ec052f35a8d
pathy <pathypbmmwamba@gmail.com>
2019-05-09 14:21:19 +0200

    Crud sur DAORessource

--- sample/model/DAO/DAORessource.java
+++ sample/model/DAO/DAORessource.java
@@ -1,25 +1,69 @@
 package sample.model.DAO;
 
+import javafx.beans.binding.DoubleBinding;
 import sample.model.Ressource;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DAORessource extends DAO<Ressource> {
+
+    public DAORessource(){
+        super();
+    }
+
     @Override
     public boolean create(Ressource ressource) {
-        return false;
+        try{
+            String requete = "insert into ressource (nom, montant, date) values('"+ressource.getNom()+"','"+ressource.getMontant()+"','"+ressource.getDate()+"')";
+            this.connexion.getStmt().executeUpdate(requete);
+            return true;
+
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean update(Ressource ressource) {
-        return false;
+        try{
+            String requete = "update ressource set nom='"+ressource.getNom()+"',montant='"+ressource.getMontant()+"',date='"+ressource.getDate()+"'WHERE id="+ressource.getId();
+            this.connexion.getStmt().executeUpdate(requete);
+            return true;
+
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean delete(Ressource ressource) {
-        return false;
+        try{
+            String requete = "delete from ressource WHERE id="+ressource.getId();
+            this.connexion.getStmt().executeUpdate(requete);
+            return true;
+
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public Ressource find(int id) {
+        Ressource res;
+        try{
+            String requete = "select * from ressource WHERE id="+id;
+            ResultSet result = this.connexion.getStmt().executeQuery(requete);
+            if(result.first()){
+                res = new Ressource(Integer.parseInt(result.getString("id")), result.getString("nom"), Double.parseDouble(result.getString("montant")), result.getString("date"));
+                return res;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
         return null;
     }
 }
