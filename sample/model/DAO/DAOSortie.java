1b65fac8851853ae55bea3a46942bedd1eddcc54
pathy <pathypbmmwamba@gmail.com>
2019-05-09 14:03:13 +0200

    Crud sur DAOSortie

--- sample/model/DAO/DAOSortie.java
+++ sample/model/DAO/DAOSortie.java
@@ -2,24 +2,77 @@ package sample.model.DAO;
 
 import sample.model.Sortie;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DAOSortie extends DAO<Sortie> {
+
+    public DAOSortie(){
+        super();
+    }
     @Override
     public boolean create(Sortie sortie) {
-        return false;
+        try{
+            String query = "insert into sortie (idcategorie, idexercice, montant) values('"+sortie.getIdCategorieSortie()+"','"+sortie.getIdExercice()+"','"+sortie.getMontant()+"')";
+            this.connexion.getStmt().executeUpdate(query);
+            return true;
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean update(Sortie sortie) {
-        return false;
+        try{
+            String query = "update sortie set idcategorie='"+
+                    sortie.getIdCategorieSortie()+"',idexercice='"+
+                    sortie.getIdExercice()+"',montant='"+sortie.getMontant()+"' WHERE id="+sortie.getId();
+            this.connexion.getStmt().executeUpdate(query);
+            return true;
+
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean delete(Sortie sortie) {
-        return false;
+        try{
+            String query = "delete from sortie WHERE id="+sortie.getId();
+            this.connexion.getStmt().executeUpdate(query);
+            return true;
+
+        }catch(SQLException e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public Sortie find(int id) {
+        Sortie sortie;
+        try{
+            String query = "select * from sortie WHERE id="+id;
+            ResultSet result = this.connexion.getStmt().executeQuery(query);
+            if(result.first()){
+                sortie = new Sortie(Integer.parseInt(result.getString("id")), Integer.parseInt(result.getString("idcategorie")), Integer.parseInt(result.getString("idexercice")), Double.parseDouble(result.getString("montant")));
+                return sortie;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
         return null;
+    }
+
+    public static void main(String[] args){
+        DAOSortie srt = new DAOSortie();
+        Sortie s = new Sortie(2, 2, 3, 4550);
+        if(srt.delete(s)){
+            System.out.println("delete fonctionnelle !!");
+        }else{
+            System.out.println("Echec delete !!!");
+        }
     }
 }
